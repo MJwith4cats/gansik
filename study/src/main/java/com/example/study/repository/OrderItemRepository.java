d58a6571b0c066cd5095a14f991c718cdeed593d
Woo Minji <life.exhib@gmail.com>
2024-09-02 10:23:22 +0900

    second commit

--- study/src/main/java/com/example/study/repository/OrderItemRepository.java
+++ study/src/main/java/com/example/study/repository/OrderItemRepository.java
@@ -3,6 +3,5 @@ package com.example.study.repository;
 import com.example.study.domain.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository <OrderItem, Long> {
-
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
